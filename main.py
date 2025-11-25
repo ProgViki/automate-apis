3fc6a79febe7b2676b83f559d64ec509783cc410
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-11-26 00:33:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from fastapi import FastAPI
+import models
+from database import engine, Base
+from routers import users, telco
+from fastapi.middleware.cors import CORSMiddleware
+
+# create DB tables
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Telco Automation API")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # tighten this in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users.router)
+app.include_router(telco.router)
 
